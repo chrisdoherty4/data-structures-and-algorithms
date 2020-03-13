605367943569c9974a50ed77200bb25651217b3a
Chris Doherty <chris.doherty4@gmail.com>
2020-03-13 16:46:06 -0600

    Complete LinkedList and add Stack files.

--- cpp/LinkedList.cc
+++ cpp/LinkedList.cc
@@ -2,25 +2,22 @@
 #include "dsa/LinkedList.hpp"
 
 template <typename T>
-void LinkedList<T>::append(T& v)
+void LinkedList<T>::add(T v)
 {
-
     node_ptr n = std::make_shared<Node>(v);
 
     if (first == NULL) {
         first = n;
+        last = first;
         return;
     }
 
-    node_ptr temp = first;
-    while (temp.next != NULL)
-        temp = temp->next;
-
-    temp->next = n;
+    last->next = n;
+    last = n;
 }
 
 template <typename T>
-void LinkedList<T>::remove(T& v)
+void LinkedList<T>::remove(T v)
 {
     if (first->value == v) {
         first = first->next;
@@ -31,11 +28,28 @@ void LinkedList<T>::remove(T& v)
     while (temp->next != nullptr && temp->next->value != v)
         temp = temp->next;
 
-    if (temp->next != nullptr) {
-        if (temp->next->next == nullptr) {
-            temp->next = nullptr;
-        } else {
-            temp->next = temp->next->next;
-        }
+    // If we've hit the end of the linked list then return because
+    // the element didn't exist.
+    if (temp->next == nullptr)
+        return;
+
+    temp->next = temp->next->next;
+    
+    if (temp->next == nullptr)
+        last = temp;
+}
+
+template <typename T>
+typename LinkedList<T>::node_ptr LinkedList<T>::find(T v)
+{
+    if (first->value == v) {
+        return first->value;
     }
+
+    node_ptr p = first;
+    
+    while (p->next != nullptr && p->next != v)
+        p = p->next;
+
+    return p->next;
 }
--- cpp/include/dsa/LinkedList.hpp
+++ cpp/include/dsa/LinkedList.hpp
@@ -1,8 +1,12 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
 #include <memory>
 
 template <typename T>
 struct LinkedList
 {
+public:
     struct Node;
     typedef std::shared_ptr<Node> node_ptr;
 
@@ -11,13 +15,20 @@ struct LinkedList
         T value;
         node_ptr next;
 
-        Node(T& value) : value(value), next(nullptr) {}
+        Node(T value) : value(value), next(nullptr) {}
     };
 
     node_ptr first;
-
+    node_ptr last;
+    
     LinkedList() : first(nullptr) {}
     ~LinkedList() {}
-    void append(T&);
-    void remove(T&);
+
+    void add(T value);
+
+    node_ptr find(T value);
+
+    void remove(T value);
 };
+
+#endif
